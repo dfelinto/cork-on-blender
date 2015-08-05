730b86af60a9f24504329039dd5fb952d98c47c5
Dalai Felinto <dfelinto@gmail.com>
2015-08-05 09:38:08 -0300

    Only Apply the Decimate modifier if factor is not 1.0

--- view3d_forensic_reconstruction/skull.py
+++ view3d_forensic_reconstruction/skull.py
@@ -14,17 +14,18 @@ def skull_import(context, filepath, decimate_factor):
     if res != {'FINISHED'}:
         raise ImportSkullException()
 
-    skull = context.object
-    decimate = skull.modifiers.new('Decimate', type='DECIMATE')
-    decimate.ratio = decimate_factor
+    if decimate_factor < 1.0:
+        skull = context.object
+        decimate = skull.modifiers.new('Decimate', type='DECIMATE')
+        decimate.ratio = decimate_factor
 
-    # apply the modifier
-    res = {'CANCELLED'}
-    if bpy.ops.object.modifier_apply.poll():
-        res = bpy.ops.object.modifier_apply(apply_as='DATA', modifier='Decimate')
+        # apply the modifier
+        res = {'CANCELLED'}
+        if bpy.ops.object.modifier_apply.poll():
+            res = bpy.ops.object.modifier_apply(apply_as='DATA', modifier='Decimate')
 
-    if res != {'FINISHED'}:
-        raise ImportSkullDecimateException(skull.name)
+        if res != {'FINISHED'}:
+            raise ImportSkullDecimateException(skull.name)
 
     # expand the view to the new object
     res = {'CANCELLED'}
